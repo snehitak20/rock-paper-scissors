cf15dde456f1274deaec675d285942332ca9e9a5
alex hernandez <alexanderohernandez21@gmail.com>
2022-03-30 12:23:47 -0700

    pseudo code

---
+++ rps.js
@@ -0,0 +1,50 @@
+
+
+
+var person = alert("let's play rock paper scissors");
+
+var choose = prompt("pick rock paper scissors")
+
+// what do we need to track.
+// input from user
+var userChoice ="";
+var computerChoice ="";
+// input  from computer(random)
+// keep track  of score
+// amount of wins,losses,tie
+var wins =0;
+var losses =0;
+var ties =0;
+// what do we need to do  
+// capture user input
+function captureUserInput(){
+    // have computer generate choice 
+}
+
+function alertChoices(){
+    // log user and computer choice
+}
+// compare choices
+function compareChoices(){
+    //rock beats scissors 
+    // scissors beats paper
+    // paper beats rock
+    // same =  same 
+}
+    
+// increment wins 
+function incrementWins(){
+
+}
+// increment losses
+function losses(){
+
+}
+// increment ties
+function ties(){
+
+}
+// confirm play again
+ 
+// play game 
+
